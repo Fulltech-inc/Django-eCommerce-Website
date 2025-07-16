319148213b28b8147efd0783b2743b675b3564b9
Spencer M. Tasa <spencertasatech@gmail.com>
2025-07-16 15:19:00 +0200

    `Updated CSS files with new color scheme: #fa3434, #fa5555, #f55555, #fa2222, #fa3434, #fa3434, #fa3434, #fa3434, #

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG")
 
-ALLOWED_HOSTS = ['192.168.0.194']
+ALLOWED_HOSTS = ['192.168.0.194', '127.0.0.1']
 
 
 # Application definition
