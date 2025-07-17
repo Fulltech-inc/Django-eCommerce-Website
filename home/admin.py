b2bdb99f3f534e8b9640df1c07918fbcbd7eb4b0
Spencer M. Tasa <spencertasatech@gmail.com>
2025-07-17 10:12:17 +0200

    "Added registration for Billboards and HeaderBanner models in admin.py"

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import ShippingAddress
+from .models import ShippingAddress, Billboards, HeaderBanner
 
 # Register your models here.
 
 admin.site.register(ShippingAddress)
+admin.site.register(Billboards)
+admin.site.register(HeaderBanner)
