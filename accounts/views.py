7d480e8420ceb0d004e1237ae6c11df748cb6d44
Spencer M. Tasa <spencertasatech@gmail.com>
2025-07-21 17:18:02 +0200

    `Added color variant selection and validation to add_to_cart view`

--- accounts/views.py
+++ accounts/views.py
@@ -110,17 +110,19 @@ def activate_email_account(request, email_token):
 @login_required
 def add_to_cart(request, uid):
     try:
-        variant = request.GET.get('size')
-        if not variant:
-            messages.warning(request, 'Please select a size variant!')
+        size_variant = request.GET.get('size')
+        color_variant = request.GET.get('color')
+        if size_variant in [None, "", "None"] or color_variant in [None, "", "None"]:
+            messages.warning(request, 'Please select a size and color variant before adding to the cart!')
             return redirect(request.META.get('HTTP_REFERER'))
 
         product = get_object_or_404(Product, uid=uid)
         cart, _ = Cart.objects.get_or_create(user=request.user, is_paid=False)
-        size_variant = get_object_or_404(SizeVariant, size_name=variant)
+        size_variant = get_object_or_404(SizeVariant, size_name=size_variant)
+        color_variant = get_object_or_404(ColorVariant, color_name=color_variant)
 
         cart_item, created = CartItem.objects.get_or_create(
-            cart=cart, product=product, size_variant=size_variant)
+            cart=cart, product=product, size_variant=size_variant, color_variant=color_variant)
         if not created:
             cart_item.quantity += 1
             cart_item.save()
